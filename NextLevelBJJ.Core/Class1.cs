c5d8692286e2920e272b086c49faace55248442b
Grzegorz <stysiok@o2.pl>
2018-06-24 10:36:44 +0200

    Removed unnecessary classes

--- NextLevelBJJ.Core/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace NextLevelBJJ.Core
-{
-    public class Class1
-    {
-    }
-}
--- NextLevelBJJ.Data/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace NextLevelBJJ.Data
-{
-    public class Class1
-    {
-    }
-}
